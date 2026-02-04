46b2af2131da424df0952bd442392d6e33720674
Siva Sai <22je0483@iitism.ac.in>
2026-02-04 23:34:58 +0530

    feat: Add event loop for handling the client connections

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <asm-generic/errno-base.h>
 #include <assert.h>
+#include <cstdint>
 #include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -9,7 +10,8 @@
 #include <string.h>
 #include <unistd.h>
 
-uint32_t k_max_msg = 4096;
+// 32 MB
+uint32_t k_max_msg = 32 << 20;
 
 void die(const char *msg) {
 	int err = errno;
@@ -19,7 +21,7 @@ void die(const char *msg) {
 
 void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
 
-int32_t read_full(int fd, char *buf, size_t n) {
+int32_t read_full(int fd, uint8_t *buf, size_t n) {
 	while (n > 0) {
 		// 0 for EOF and -1 for error
 		ssize_t rv = read(fd, buf, n);
@@ -39,7 +41,7 @@ int32_t read_full(int fd, char *buf, size_t n) {
 	return 0;
 }
 
-int32_t write_all(int fd, const char *buf, size_t n) {
+int32_t write_all(int fd, const uint8_t *buf, size_t n) {
 	while (n > 0) {
 		ssize_t rv = write(fd, buf, n);
 		if (rv <= 0) {
@@ -52,4 +54,12 @@ int32_t write_all(int fd, const char *buf, size_t n) {
 	}
 
 	return 0;
+}
+
+void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
+	buf.insert(buf.end(), data, data + len);
+}
+
+void buf_consume(std::vector<uint8_t> &buf, size_t n) {
+	buf.erase(buf.begin(), buf.begin() + n);
 }
